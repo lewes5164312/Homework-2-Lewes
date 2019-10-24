176ca3244775806dd6fa819b00539379a558da10
Lewes <z5164312@student.unsw.edu.au>
2019-10-24 23:40:19 +1100

    hi
    hi

---
+++ 3634hwk/app/src/main/java/com/example/borgerk/ItemListingActivity.java
@@ -0,0 +1,62 @@
+package com.example.borgerk;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ItemListingActivity extends AppCompatActivity {
+    private TextView nameTV;
+    private TextView priceTV;
+    private TextView descTV;
+    private TextView qtyTV;
+    private ImageView imageIV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_listing);
+
+        Intent intent = getIntent();
+
+        int itemID = intent.getIntExtra("ItemID", 0);
+
+        Item item = MenuItems.getMenuItemById(itemID);
+
+        nameTV = findViewById(R.id.item_listing_name_tv);
+        priceTV = findViewById(R.id.item_listing_price_tv);
+        descTV = findViewById(R.id.item_listing_desc_tv);
+        imageIV = findViewById(R.id.item_listing_image_iv);
+        qtyTV = findViewById(R.id.item_listing_qty_cart_tv);
+        nameTV.setText(item.getItemName());
+        priceTV.setText("$" + Double.toString(item.getItemPrice()));
+        descTV.setText(item.getItemDesc());
+        imageIV.setImageResource(item.getImageDrawableId());
+        qtyTV.setText("In Order: " + Integer.toString(item.getQuantityCart()));
+
+    }
+
+    public void clickincrease (View v){
+        //adds quantity when button is clicked and instantly displays update
+        Intent intent = getIntent();
+        int itemID = intent.getIntExtra("ItemID", 0);
+        Item item = MenuItems.getMenuItemById(itemID);
+        item.addQuantityCart();
+        qtyTV = findViewById(R.id.item_listing_qty_cart_tv);
+        qtyTV.setText("In Order: " + Integer.toString(item.getQuantityCart()));
+    }
+
+    public void clickdecrease(View v){
+        //removes quantity when button is clicked and instantly displays update
+        Intent intent = getIntent();
+        int itemID = intent.getIntExtra("ItemID", 0);
+        Item item = MenuItems.getMenuItemById(itemID);
+        item.removeQuantityCart();
+        qtyTV = findViewById(R.id.item_listing_qty_cart_tv);
+        qtyTV.setText("In Order: " + Integer.toString(item.getQuantityCart()));
+    }
+}
+
---
+++ 3634hwk/app/src/main/java/com/example/borgerk/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.borgerk;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Bundle;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rv_main);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ItemAdapter itemAdapter = new ItemAdapter();
+        itemAdapter.setData(MenuItems.getAllMenuItems());
+        recyclerView.setAdapter(itemAdapter);
+    }
+
+    public void viewOrderClick(View v) {
+        DecimalFormat df = new DecimalFormat("0.00");
+        setContentView(R.layout.order_details);
+        //order total calculated through a loop that sums the (cart quantity * item price) for all items
+        double ordertotal = 0;
+        int itemID = 1;
+        Item firstitem = MenuItems.getMenuItemById(1);
+        int numberofitems = firstitem.getNumberOfItems();
+        while (itemID < numberofitems+1) {
+            Item item = MenuItems.getMenuItemById(itemID);
+            ordertotal = ordertotal + (item.getQuantityCart() * item.getItemPrice());
+            itemID++;
+        }
+        TextView totalTV;
+        totalTV = findViewById(R.id.order_total);
+        totalTV.setText("Order Total: $" + df.format(ordertotal));
+
+        TextView orderTV;
+        orderTV = findViewById(R.id.order_items);
+
+        String ordertext = "Items: \n\n";
+        itemID = 1;
+
+        //loop adds to string the item, quanaity and total price of all individual items, then returns a new line
+
+        while (itemID < numberofitems+1) {
+            Item item = MenuItems.getMenuItemById(itemID);
+            if (item.getQuantityCart() > 0) {
+                double itemtotal = item.getQuantityCart() * item.getItemPrice();
+                ordertext = ordertext + Integer.toString(item.getQuantityCart()) + "x "
+                + item.getItemName() +" - $" + String.valueOf(df.format(itemtotal)) + "\n";
+            }
+            itemID++;
+        }
+        orderTV.setText(ordertext);
+
+    }
+    public void backClick(View v) {
+        //return to main screen
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rv_main);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ItemAdapter itemAdapter = new ItemAdapter();
+        itemAdapter.setData(MenuItems.getAllMenuItems());
+        recyclerView.setAdapter(itemAdapter);
+    }
+}
